35b5b941ec24474cb3bad1ca3d9ead2691b0d448
冠登 廖 <t8599008@ntut.org.tw>
2021-11-01 18:45:06 +0800

    tmp

--- 110-1MidSample/Sample2.aspx
+++ 110-1MidSample/Sample2.aspx
@@ -10,14 +10,14 @@
 <body>
     <form id="form1" runat="server">
         <div>
-            <h2>保單電訪系統</h2><br />
+            <h2>保單電訪單</h2><br />
             <asp:Label runat="server" Text="保單編號" Font-Size="X-Large"></asp:Label><asp:Label ID="lb_Id" runat="server" Text=""></asp:Label>
             <asp:HiddenField ID="hf_Id" runat="server" />
             <br />
             <asp:Label runat="server" Text="聯絡方式" Font-Size="X-Large"></asp:Label>
-            <asp:DropDownList ID="ddl_Type" runat="server"></asp:DropDownList>
+            <asp:DropDownList ID="ddl_Type" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddl_Type_SelectedIndexChanged"></asp:DropDownList>
             <br />
-            <asp:DropDownList ID="ddl_ZoneCode" runat="server"></asp:DropDownList>
+            <asp:DropDownList ID="ddl_ZoneCode" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddl_ZoneCode_SelectedIndexChanged"></asp:DropDownList>
             <asp:TextBox ID="tb_Number" runat="server"></asp:TextBox>
             <br />
             <asp:Label runat="server" Text="地址" Font-Size="X-Large"></asp:Label>
@@ -25,8 +25,8 @@
             <asp:TextBox ID="tb_Add" runat="server"></asp:TextBox>
             <br />
             <asp:Label runat="server" Text="說明" Font-Size="X-Large"></asp:Label>
-            <asp:RadioButtonList ID="rb_Des" runat="server">
-                <asp:ListItem>否</asp:ListItem>
+            <asp:RadioButtonList ID="rb_Des" runat="server" AutoPostBack="true" OnSelectedIndexChanged="rb_Des_SelectedIndexChanged">
+                <asp:ListItem Selected="True">否</asp:ListItem>
                 <asp:ListItem>是</asp:ListItem>
             </asp:RadioButtonList>
             <asp:TextBox ID="tb_Des" runat="server"></asp:TextBox>
--- 110-1MidSample/Sample2.aspx.cs
+++ 110-1MidSample/Sample2.aspx.cs
@@ -7,14 +7,27 @@ using System.Web.UI.WebControls;
 
 namespace _110_1MidSample {
     public partial class Sample2 : System.Web.UI.Page {
+        public string[] sa_Type = new string[2] {"手機", "市話"};
+        public string[,] sa_ZoneCode = new string[2,3] { {"0911", "0921", "0960"}, {"02", "04", "07"} };
+        public string[] sa_City = new string[6] {"台北市","新北市","台中市","台中縣","高雄市","高雄縣"};
+
         protected void Page_Load(object sender, EventArgs e) {
             if (IsPostBack == false) {
                 string s_Id = mt_GenVeriStr();
                 lb_Id.Text = s_Id;
                 hf_Id.Value = s_Id;
 
-            }
 
+                // Add Type
+                for (int i_Ct=0; i_Ct< sa_Type.Length; i_Ct++) {
+                    ListItem o_It = new ListItem(sa_Type[i_Ct], sa_Type[i_Ct]);
+                    ddl_Type.Items.Add(o_It);
+                }
+
+                mt_UpdateLayer();
+
+                mt_ShowDes();
+            }
         }
 
         // To generate a 4 digital number
@@ -26,6 +39,80 @@ namespace _110_1MidSample {
                 s_Res = s_Res + i_Tmp.ToString();
             }
             return s_Res;
+        }
+
+
+        public void mt_UpdateLayer(int i_Layer = 2) {
+            if (i_Layer == 2) {
+                // Add ZoneCode
+                ddl_ZoneCode.Items.Clear();
+                if (ddl_Type.SelectedValue == "手機") {
+                    for (int i_Ct = 0; i_Ct < sa_ZoneCode.GetLength(1); i_Ct++) {
+                        ListItem o_It = new ListItem(sa_ZoneCode[0, i_Ct], sa_ZoneCode[0, i_Ct]);
+                        ddl_ZoneCode.Items.Add(o_It);
+                    }
+                }
+                else {
+                    for (int i_Ct = 0; i_Ct < sa_ZoneCode.GetLength(1); i_Ct++) {
+                        ListItem o_It = new ListItem(sa_ZoneCode[1, i_Ct], sa_ZoneCode[1, i_Ct]);
+                        ddl_ZoneCode.Items.Add(o_It);
+                    }
+                }
+            }
+
+            // Add City
+            ddl_City.Items.Clear();
+            //ddl_ZoneCode.SelectedValue
+            bool b_IsFRow = false;
+            int i_Row = -1;
+            for (int i_Ct = 0; i_Ct < sa_ZoneCode.GetLength(1); i_Ct++) {
+                if (sa_ZoneCode[0, i_Ct] == ddl_ZoneCode.SelectedValue) {
+                    b_IsFRow = true;
+                    break;
+                }
+            }
+
+            for (int i_Ct = 0; i_Ct < sa_ZoneCode.GetLength(1); i_Ct++) {
+                if (sa_ZoneCode[1, i_Ct] == ddl_ZoneCode.SelectedValue) {
+                    i_Row = i_Ct;
+                    break;
+                }
+            }
+
+
+            if (b_IsFRow == true) {
+                for (int i_Ct = 0; i_Ct < sa_ZoneCode.Length; i_Ct++) {
+                    ListItem o_It = new ListItem(sa_City[i_Ct], sa_City[i_Ct]);
+                    ddl_City.Items.Add(o_It);
+                }
+            }
+            else {
+                for (int i_Ct = i_Row * 2; i_Ct < (i_Row * 2) + 2; i_Ct++) {
+                    ListItem o_It = new ListItem(sa_City[i_Ct], sa_City[i_Ct]);
+                    ddl_City.Items.Add(o_It);
+                }
+            }
+        }
+
+        protected void ddl_Type_SelectedIndexChanged(object sender, EventArgs e) {
+            mt_UpdateLayer();
+        }
+
+        protected void ddl_ZoneCode_SelectedIndexChanged(object sender, EventArgs e) {
+            mt_UpdateLayer(1);
+        }
+
+        public void mt_ShowDes() {
+            if (rb_Des.SelectedValue == "否") {
+                tb_Des.Visible = false;
+            }
+            else {
+                tb_Des.Visible = true;
+            }
+        }
+
+        protected void rb_Des_SelectedIndexChanged(object sender, EventArgs e) {
+            mt_ShowDes();
         }
     }
 }
